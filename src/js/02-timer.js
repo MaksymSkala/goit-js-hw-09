2a644da88928b79fe20fa5cc1047170d3026bb94
MaksymSkala <maksymskala@gmail.com>
2023-08-22 17:16:06 +0300

    add

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,65 +1,7 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-// Define the convertMs function
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
-
-const datetimePicker = document.querySelector("#datetime-picker");
-const startButton = document.querySelector('[data-start]');
-const timerFields = {
-  days: document.querySelector('[data-days]'),
-  hours: document.querySelector('[data-hours]'),
-  minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]')
-};
-
-let countdownIntervalId = null;
-
-startButton.addEventListener('click', () => {
-  startButton.disabled = true;
-  const selectedDate = new Date(datetimePicker.value);
-
-  countdownIntervalId = setInterval(() => {
-    const timeLeft = selectedDate - new Date();
-    if (timeLeft <= 0) {
-      clearInterval(countdownIntervalId);
-      updateTimerFields(0);
-      return;
-    }
-    updateTimerFields(timeLeft);
-  }, 1000);
-});
-
-function updateTimerFields(ms) {
-  const { days, hours, minutes, seconds } = convertMs(ms);
-
-  timerFields.days.textContent = addLeadingZero(days);
-  timerFields.hours.textContent = addLeadingZero(hours);
-  timerFields.minutes.textContent = addLeadingZero(minutes);
-  timerFields.seconds.textContent = addLeadingZero(seconds);
-}
-
-function addLeadingZero(value) {
-  return value.toString().padStart(2, '0');
-}
-
+// Описаний в документації
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -67,12 +9,55 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
-    if (selectedDate <= new Date()) {
-      notiflix.Notify.failure("Please choose a date in the future");
+    const currentDate = new Date();
+    if (selectedDate <= currentDate) {
+      alert("Please choose a date in the future");
       return;
     }
     startButton.disabled = false;
   },
 };
 
-flatpickr(datetimePicker, options);
+const datePicker = document.querySelector("#datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const timerFields = document.querySelectorAll(".value");
+
+flatpickr(datePicker, options);
+
+let countdownInterval;
+
+function startCountdown(targetDate) {
+  countdownInterval = setInterval(() => {
+    const timeLeft = targetDate - new Date();
+    if (timeLeft <= 0) {
+      clearInterval(countdownInterval);
+      updateTimerDisplay(0);
+      alert("Countdown has finished!");
+      return;
+    }
+    updateTimerDisplay(timeLeft);
+  }, 1000);
+}
+
+function updateTimerDisplay(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+  timerFields[0].textContent = addLeadingZero(days);
+  timerFields[1].textContent = addLeadingZero(hours);
+  timerFields[2].textContent = addLeadingZero(minutes);
+  timerFields[3].textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
+
+startButton.addEventListener("click", () => {
+  const selectedDate = datePicker._flatpickr.selectedDates[0];
+  const currentDate = new Date();
+  if (selectedDate <= currentDate) {
+    alert("Please choose a date in the future");
+    return;
+  }
+  startCountdown(selectedDate);
+  startButton.disabled = true;
+});
