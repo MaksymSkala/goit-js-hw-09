d121647539e2789cf5a2b3079ae5734ff84a05e1
MaksymSkala <maksymskala@gmail.com>
2023-08-22 16:30:48 +0300

    add

---
+++ src/js/02-timer.js
@@ -0,0 +1,59 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const datetimePicker = document.querySelector("#datetime-picker");
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        const selectedDate = selectedDates[0];
+        if (selectedDate <= new Date()) {
+        notiflix.Notify.failure("Please choose a date in the future");
+        return;
+        }
+        startButton.disabled = false;
+    },
+};
+
+flatpickr(datetimePicker, options);
+
+const startButton = document.querySelector('[data-start]');
+const timerFields = {
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]')
+};
+
+let countdownIntervalId = null;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    const selectedDate = new Date(datetimePicker.value);
+
+    countdownIntervalId = setInterval(() => {
+        const timeLeft = selectedDate - new Date();
+        if (timeLeft <= 0) {
+        clearInterval(countdownIntervalId);
+        updateTimerFields(0);
+        return;
+        }
+        updateTimerFields(timeLeft);
+    }, 1000);
+});
+
+function updateTimerFields(ms) {
+    const { days, hours, minutes, seconds } = convertMs(ms);
+
+    timerFields.days.textContent = addLeadingZero(days);
+    timerFields.hours.textContent = addLeadingZero(hours);
+    timerFields.minutes.textContent = addLeadingZero(minutes);
+    timerFields.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+}
